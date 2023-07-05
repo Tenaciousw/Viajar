166de6ee4ea7e46200ce90857b5c1e365998a6e0
CPetruzzo <njw@live.com.ar>
2023-07-05 18:15:03 -0300

    Detalle Bus

--- src/app/modules/person-administration/person-administration.module.ts
+++ src/app/modules/person-administration/person-administration.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonDetailComponent } from './person-detail/person-detail.component';
-// import { FormsModule } from "@angular/forms";
 import { PersonListComponent } from './person-list/person-list.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {PersonAdministrationRoutingModule} from "./person-administration-routing.module";
