afd4fb45ddcb279d328ffaee5c996b60ae954e6d
CPetruzzo <njw@live.com.ar>
2023-07-04 21:57:59 -0300

    creacion componente busesdetail y ruteo buses

--- src/app/modules/buses-administration/buses-administration-routing.module.ts
+++ src/app/modules/buses-administration/buses-administration-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BusesListComponent} from "./buses-list/buses-list.component";
+import { BusesDetailComponent } from './buses-detail/buses-detail.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'list', pathMatch: "full"},
-  {path: 'list', component: BusesListComponent}
-];
-
+  {path: '', redirectTo: 'list', pathMatch: 'full'},
+  {path: 'list', component: BusesListComponent},
+  {path: 'create', component: BusesDetailComponent},
+  {path: 'detail/:id', component: BusesDetailComponent}
+]
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
