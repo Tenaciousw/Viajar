8d6e35c90be84175f205003848dec341c1fb11df
CPetruzzo <njw@live.com.ar>
2023-07-05 16:08:34 -0300

    Bus Form

--- src/app/modules/buses-administration/buses-detail/buses-detail.component.ts
+++ src/app/modules/buses-administration/buses-detail/buses-detail.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-buses-detail',
   templateUrl: './buses-detail.component.html',
   styleUrls: ['./buses-detail.component.css']
 })
-export class BusesDetailComponent {
+export class BusesDetailComponent implements OnInit{
 
+  busform = this.formBuilder.group( {
+    patente: ['', Validators.required],
+    cantidadAsientos: ['', Validators.required, Validators.min(0), Validators.max(50)],
+    Modeloid: ['', Validators.required, Validators.min(0), Validators.max(50)]
+  })
+      
+  constructor(private formBuilder: FormBuilder) {
+    
+  }
+  ngOnInit() {
+
+  }
+  
 }
