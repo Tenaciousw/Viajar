0b8158c9ed6d6831d2fd8dc7a3648ba0fe67c6ae
CPetruzzo <njw@live.com.ar>
2023-07-05 00:04:41 -0300

    cambios bus

--- src/app/modules/buses-administration/buses-administration.module.ts
+++ src/app/modules/buses-administration/buses-administration.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { BusesAdministrationRoutingModule } from './buses-administration-routing.module';
 import {BusesListComponent} from "./buses-list/buses-list.component";
+import { BusesDetailComponent } from './buses-detail/buses-detail.component';
 
 
 @NgModule({
-  declarations: [BusesListComponent],
+  declarations: [BusesListComponent, BusesDetailComponent],
   imports: [
     CommonModule,
     BusesAdministrationRoutingModule
---
+++ src/app/modules/buses-administration/buses-detail/buses-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buses-detail',
+  templateUrl: './buses-detail.component.html',
+  styleUrls: ['./buses-detail.component.css']
+})
+export class BusesDetailComponent {
+
+}
